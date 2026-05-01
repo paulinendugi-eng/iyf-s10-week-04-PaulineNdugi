e80a5cc017481a6fbfe11ee5cf516d33afc1e003
paulinendugi-eng <paulinendugi@gmail.com>
2026-05-01 23:38:32 +0000

    Add JavaScript variable practice file

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+let name ="Pauline";
+let age = 22;
+let isStudent = true;
+let favouriteColors = ["blue", "black", "whtite"];
+let todayDate = new Date();
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Am I a student?", isStudent);
+console.log("Favourite colors:", favouriteColors);
+console.log("Today's date:", todayDate);
 
